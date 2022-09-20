4825baa981e8429c142d90d69398723eb0b22421
maaz44hello <79090862+maaz44hello@users.noreply.github.com>
2022-09-20 13:54:44 +0530

    Add files via upload

---
+++ screens/camera.js
@@ -0,0 +1,82 @@
+import * as React from "react";
+import {Button,Image,View,Platform} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Permission from "expo-permissions";
+
+export default class PickImage extends React.Component{
+
+    state = {
+        image: null,
+    };
+    render(){
+        let { image } = this.state;
+
+        return(
+            <View style={{ flex:1, alignItems: "center",justifyContent:"center"}}>
+                <Button
+                title="Pick an image from camera roll"
+                onPress={this._pickImage}
+                />
+            </View>
+        );
+     }
+
+     componentDidMount() {
+        this.getPermissionAsync();
+      }
+//asking for permission and checking accepted or denied
+     getPermissionAsync = async () => {
+        if (Platform.OS !== "web") {
+          const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+          if (status !== "granted") {
+            alert("Sorry, we need camera roll permissions to make this work!");
+          }
+        }
+      };
+//for uploading image 
+      uploadImage = async (uri) => {
+        const data = new FormData();
+        let filename = uri.split("/")[uri.split("/").length - 1]
+        let type = `image/${uri.split('.')[uri.split('.').length - 1]}`
+        const fileToUpload = {
+          uri: uri,
+          name: filename,
+          type: type,
+        };
+        data.append("digit", fileToUpload);
+        //post request
+        fetch("https://d744-2405-204-a013-f889-ddbf-5e45-e0af-fef4.in.ngrok.io/predict-digit", {
+          method: "POST",
+          body: data,
+          headers: {
+            "content-type": "multipart/form-data",
+          },
+        })
+          .then((response) => response.json())
+          .then((result) => {
+            console.log("Success:", result);
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      };
+//pick image from device gallery 
+      _pickImage = async () => {
+        try {
+          let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+          });
+          if (!result.cancelled) {
+            this.setState({ image: result.data });
+            console.log(result.uri)
+            this.uploadImage(result.uri);
+          }
+        } catch (E) {
+          console.log(E);
+        }
+      };
+}
+
